5f47b649a225aceaa5f095522583472d7262b360
Ruzhikov-Sasha-Maksimovich <109194754+Ruzhikov-Sasha-Maksimovich@users.noreply.github.com>
2022-08-19 13:23:05 +0800

    Commit second

--- Project_05/Program.cs
+++ Project_05/Program.cs
@@ -1,6 +1,14 @@
-﻿System.Console.WriteLine("Введите число: ");
-int chislo = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите число: ");
+int chislo = 0;
 int sum = 0;
+int count = int.Parse(Console.ReadLine()!);
 
+while(chislo !=  count)
+{
+sum = chislo * chislo * chislo;
+System.Console.WrsiteLine(sum);
+chislo = chislo + 1;
+sum = sum - sum;
+}
 sum = chislo * chislo * chislo;
 System.Console.WriteLine(sum);
